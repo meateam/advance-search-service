316107a19ae2f370674afce5db69277962091dc4
Liora Yacob <liorayacob10@gmail.com>
2020-12-20 15:59:41 +0200

    feat: Working Search Query

--- search_client.js
+++ search_client.js
@@ -2,27 +2,57 @@ var PROTO_PATH = __dirname + '/search.proto';
 
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
+
 var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
+  PROTO_PATH,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  });
+
 var search_proto = grpc.loadPackageDefinition(packageDefinition).searchService;
 
+//Example ----->
+
+// const fileMetaData = {
+//   fileName: "crow",
+//   userId: 'https://cdn.elastic-elastic-elastic.org/styles/app.css',
+//   createdAt: {
+//     start: "252600",
+//     end: "2524039936732762600"
+//   },
+//   updatedAt: {
+//     start: "9322000",
+//     end: "7204335048999322000"
+//   },
+//   type: "xl",
+//   content: "sober",
+//   owner: {
+//     hierarchy: "kristopher",
+//     name: "McKenzie",
+//     userId: "027-16-6961"
+//   },
+//   permissions: "Gottlieb"
+// }
+
 function main() {
-    var client = new search_proto.Search('localhost:50051',
-                                         grpc.credentials.createInsecure());
+  var client = new search_proto.Search('localhost:8000',
+    grpc.credentials.createInsecure());
 
+  //TODO-  permission part before sending the search request
+  //sending with the search request ---  1) Indexes Array of userID      2)fields (MetaData)
 
-    client.search({userId: 'https://cdn.elastic-elastic-elastic.org/styles/app.css', fileds:["a", "b"], value: 'GET'}, function(err, response) {
-      if(err){
-        return console.log('Error:', err.message);
-      }
-        console.log('Found:', response.message);
-      });
-  }
+  client.search({ userId: 'https://cdn.elastic-elastic-elastic.org/styles/app.css', fields: fileMetaData }, function (err, response) {
 
-  main();
+    if (err) {
+      return console.log('Error:', err.message);
+    }
+
+    console.log('response: ', response);
+  });
+}
+
+main();
--- search_server.js
+++ search_server.js
@@ -12,74 +12,131 @@ const packageDefinition = protoLoader.loadSync(
         oneofs: true
     });
 const search_proto = grpc.loadPackageDefinition(packageDefinition).searchService;
-const client = new Client({ node: 'http://localhost:9200' });
-
-async function search(call, callback) {
-    try {
-        const result = await client.search({
-            index: 'kibana_sample_data_logs',
-            body: {
-                // "query": {
-                //         "match": {
-                //             "message": call.request.value
-                //         },
-                //         "match": {
-                //             "host": call.request.userId
-                //         },
-                //         "range": {
-                //             "@timestamp": {
-                //               "gte": "2001-01-01T00:00:00.000+02:00",
-                //               "lt": "2001-04-01T00:00:00.000+02:00"
-                //             }
-                //           }
-                // },
-                
-                    "query": {
-                      "bool": {
-                        "must": [
-                          {
-                            "range": {
-                              "timestamp": {
-                                "gte": 633886648125,
-                                "lte": 1606058904286,
-                                "format": "epoch_millis"
-                              }
-                            }
-                          },
-                          {
-                            "match": {
-                                "message": "GET"
-                            }
-                          }
-                        ],
-                        "filter": [],
-                        "should": [],
-                        "must_not": []
-                      }
-                    },
-                    "size":20,
-                "_source": {
-                    "includes": ["url"]
-                }
-            }
-        })
-        const fileIds = result.body.hits.hits.map(document => document._source.url)
-        callback(null, { message: fileIds });
-    }
-    catch (err) {
-        console.log(err)
-        callback(err, null);
-    }
-}
+const clientES = new Client({ node: 'http://localhost:9200' });
 
 
 function main() {
-    var server = new grpc.Server();
-    server.addService(search_proto.Search.service, { search: search });
-    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-        server.start();
-    });
-
+  var server = new grpc.Server();
+  server.addService(search_proto.Search.service, { search: search });
+  server.bindAsync('0.0.0.0:8000', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+  });
 }
 
 main();
+
+async function search(call, callback) {
+  try {
+    let fields = call.request.fields;
+    console.log(fields);
+    let indexesArray = new Array("liora", "lior2");
+    console.log(indexesArray);
+
+    const result = await clientES.search({
+      index: indexesArray,
+      body: {
+        "from": 0, "size": 20,  //max number of results 
+        "query": {
+          "bool": {
+            "must": [
+              {
+                "query_string": {
+                  "default_field": "fileName",
+                  "query": `*${fields.fileName}*`
+                }
+              },
+              {
+                "query_string": {
+                  "default_field": "type",
+                  "query": `*${fields.type}*`
+                }
+              },
+              {
+                "query_string": {
+                  "default_field": "owner.name",
+                  "query": `*${fields.owner.name}*`
+                }
+              },
+              {
+                "query_string": {
+                  "default_field": "owner.hierarchy",
+                  "query": `*${fields.owner.hierarchy}*`
+                }
+              },
+              {
+                "query_string": {
+                  "default_field": "content",
+                  "query": `* ${fields.content}*`
+                }
+              },
+              {
+                "range": {
+                  "createdAt": {
+                    "gte": `${fields.createdAt.start}`,
+                    "lte": `${fields.createdAt.end}`
+                  }
+                }
+              },
+              {
+                "range": {
+                  "updatedAt": {
+                    "gte": `${fields.updatedAt.start}`,
+                    "lte": `${fields.updatedAt.end}`
+                  }
+                }
+              },
+              {
+                "nested": {
+                  "path": "permissions",
+                  "query": {
+                    "bool": {
+                      "should": [
+                        {
+                          "query_string": {
+                            "default_field": "permissions.user.hierarchy",
+                            "query": `*${fields.permissions}*`
+                          }
+                        },
+                        {
+                          "query_string": {
+                            "default_field": "permissions.user.name",
+                            "query": `*${fields.permissions}*`
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        },
+        "highlight": {
+          "pre_tags": ["<b>"],
+          "post_tags": ["</b>"],
+          "fields": {
+            "content": {}
+          }
+        }
+      }
+    }
+    );
+
+    const fileIds = result.body.hits.hits.map(document =>  document._source.fileId);
+
+    const highlight = result.body.hits.hits.map(document => {
+      if (document.highlight) {
+        return document.highlight.content;
+      }
+    }).join(' ');
+
+    // console.log("fileIds: " + fileIds);
+    // console.log("highlight: " + highlight);
+
+    callback(null, { fileIds: fileIds, highlightedContent:highlight });
+  }
+  catch (err) {
+    console.log(err)
+    callback(err, null);
+  }
+}
